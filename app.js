2b8d3edc71ab92dc835780a09fcb016aad90746c
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-06-16 23:14:00 +0900

    리디렉션 경로 설정

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      domain: "localhost:3000", // .ysizuku.com으로 설정하면 모든 서브도메인에서 쿠키를 사용할 수 있습니다.
+      domain: ".ysizuku.com", // .ysizuku.com으로 설정하면 모든 서브도메인에서 쿠키를 사용할 수 있습니다.
       path: "/", // /로 설정하면 모든 페이지에서 쿠키를 사용할 수 있습니다.
       secure: false, // https가 아닌 환경에서도 사용할 수 있습니다.
       httpOnly: false, // 자바스크립트에서 쿠키를 확인할 수 있습니다.
