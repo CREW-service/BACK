d2586f77ce2ff3210aea519cbc8ab4567c62b3db
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-06-24 19:00:47 +0900

    currentUser 추가

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const jwt = require("jsonwebtoken");
+const authJwt = require("../middlewares/authMiddleware");
 
 //로그인
 router.post("/login", async (req, res) => {
@@ -22,6 +23,17 @@ router.post("/login", async (req, res) => {
   } catch (error) {
     console.log("error : ", error);
     return res.json({ errorMessage: "요청이 올바르지 않습니다." } + error);
+  }
+});
+
+// userId 넣어주는 API
+router.get("/currentUser", authJwt, async (req, res) => {
+  try {
+    const { userId } = res.locals.user;
+    return res.status(200).json({ userId });
+  } catch (e) {
+    console.log("currentUser error : ", e);
+    return res.status(400).json({ errorMessage: "userId 전달 실패" });
   }
 });
 
