61448f260769eb58237246dd80785017d6133fa5
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-06-25 12:35:35 +0900

    mypage.js 수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authJwt = require("../middlewares/authMiddleware");
-const { Boats, Crews } = require("../models");
+const { Users, Boats, Crews } = require("../models");
 
 /* mypage API
 토큰을 검사하여 userId에 맞게 모집 글, 참여 글 불러오기 */
@@ -9,10 +9,14 @@ router.get("/mypage", authJwt, async (req, res) => {
   try {
     // user 정보
     const { userId } = res.locals.user;
+    const user = await Users.findOne({
+      where: { userId },
+      raw: true,
+    });
 
     // userId에 맞춰 작성한 글 가져오기
     const writedBoatsPromise = await Boats.findAll({
-      attributes: ["boatId", "title", "createdAt", "isDone"],
+      attributes: ["boatId", "captain", "title", "createdAt", "isDone"],
       where: { userId, deletedAt: null },
       order: [["createdAt", "DESC"]],
       raw: true,
@@ -36,7 +40,7 @@ router.get("/mypage", authJwt, async (req, res) => {
       const boatId = attendedBoatsData[i].boatId;
 
       const boatPromise = await Boats.findOne({
-        attributes: ["boatId", "title", "createdAt", "isDone"],
+        attributes: ["boatId", "captain", "title", "createdAt", "isDone"],
         where: { boatId, deletedAt: null },
         raw: true,
       });
@@ -49,6 +53,7 @@ router.get("/mypage", authJwt, async (req, res) => {
     }
 
     return res.status(200).json({
+      user,
       writedBoats,
       attendedBoats: attendedBoats,
     });
