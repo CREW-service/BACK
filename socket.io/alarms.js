2ff20c8b8f4cf8f17cc1ff254f588c78202e70fb
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-07-03 10:49:34 +0900

    socket 부분 수정

--- socket.io/alarms.js
+++ socket.io/alarms.js
@@ -3,11 +3,27 @@ const { Alarms } = require("../models");
 module.exports = (io) => {
   // socket.io 연결
   io.on("connetion", (socket) => {
-    socket.onAny((event) => {
-      console.log(`Socket Event: ${event}`);
+    socket.on("login", async (userId) => {
+      console.log(`User ${userId} logged in.`);
+
+      try {
+        // 알람 가져오기
+        const alarms = await Alarms.findAll({ where: { userId } });
+        // 알람을 해당 user에게 전송
+        socket.emit("alarms", alarms);
+        console.log("알람 전송 완료");
+      } catch (e) {
+        console.error(`socket 알람 전송 요청이 올바르지 않습니다.`, e);
+      }
     });
 
-    // userId 체크
-    async;
+    socket.on("alarmRead", async (alarmId) => {
+      try {
+        await Alarms.updateOne({ alarmId }, { isRead: true });
+        socket.emit("alarmReadSuccess");
+      } catch (e) {
+        console.error(`socket 읽음 처리 요청이 올바르지 않습니다.`, e);
+      }
+    });
   });
 };
