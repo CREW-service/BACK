c4ce290a7f69900b683c283e7daf5bdef5a70b4f
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-07-22 16:34:48 +0900

    수정

--- routes/auth.js
+++ routes/auth.js
@@ -18,14 +18,14 @@ router.get(
   (req, res) => {
     const token = req.user; // 사용자 토큰 정보 (예: JWT 토큰)
     const oneHour = 7200000; // 1시간 (밀리초 단위)
-    res.cookie("authorization", `Bearer ${token}`, {
-      maxAge: oneHour,
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
-
-    res.redirect(`https://crew.ysizuku.com`);
+    res
+      .cookie("authorization", `Bearer ${token}`, {
+        maxAge: oneHour,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .redirect(`https://crew.ysizuku.com`);
   }
 );
 
