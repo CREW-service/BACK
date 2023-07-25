f12ee5261e34e970d127d43caca6de42486d679a
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-07-25 19:04:53 +0900

    asdf

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,10 @@ router.get(
 
 router.get("/auth/logout", async (req, res) => {
   try {
-    res.clearCookie("authorization");
+    res.append(
+      "Set-Cookie",
+      `authorization=Bearer ${token}; Max-Age=${age}; expires=Thu, 01 Jan 1970 00:00:00 UTC; Secure; SameSite=None; Domain=.spa-mall.shop; Path=/`
+    );
     res.status(200).json({ message: "로그아웃 성공" });
   } catch (e) {
     console.error(e.message);
