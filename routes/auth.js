fc1c936fc7fa54ae2cb9cb6981e18a7a90212128
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2023-07-25 12:52:09 +0900

    as

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,7 @@ router.get("/auth/logout", async (req, res) => {
   try {
     req.session.destroy();
     res.clearCookie("authorization");
+    console.log(res.clearCookie("authorization"));
     res.status(200).json({ message: "로그아웃 성공" });
   } catch (e) {
     console.error(e.message);
